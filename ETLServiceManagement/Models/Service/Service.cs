c44fa18017175155e35a7ed5a0e132d596541139
Shehzad Amjad <74287706+ShahzadAmjad@users.noreply.github.com>
2022-04-25 13:55:38 +0500

    fields

--- ETLServiceManagement/Models/Service/Service.cs
+++ ETLServiceManagement/Models/Service/Service.cs
@@ -6,10 +6,24 @@ namespace ETLServiceManagement.Models.Service
     {
         [Key]
         public int ServiceId { get; set; }
+        [Required]
         public string ServiceName { get; set; }
+        [Required]
         public string RefreshTime { get; set; }
+        [Required]
         public string DataSource { get; set; }
+        [Required]
+        public string SourceFolder { get; set; }
+        [Required]
         public string Mapping { get; set; }
+        [Required]
+        public string DestinationDb { get; set; }
+        [Required]
+        public string DbUrl { get; set; }
+        [Required]
+        public string DbName { get; set; }
+        [Required]
+        public string TableName { get; set; }
         public string Status { get; set; }
     }
 }
