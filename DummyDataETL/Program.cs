084cc84c73f59f1a853a755606648cbe4f9a3717
Shehzad Amjad <74287706+ShahzadAmjad@users.noreply.github.com>
2022-04-26 13:59:46 +0500

    update

--- DummyDataETL/Program.cs
+++ DummyDataETL/Program.cs
@@ -12,3 +12,17 @@ new Student { studentId=3,name="Yahya Amjad", degree="BSCS",semester=8}};
 List<Employee> EmpList = new List<Employee>() { new Employee { EmployeeId=1,name="dawood", Dept="BSCS",salary=500},
 new Employee { EmployeeId=2,name="Shehzad Amjad", Dept="BSCS",salary=700},
 new Employee { EmployeeId=3,name="Yahya Amjad", Dept="BSCS",salary=900}};
+
+
+var EmployeeJson= JsonSerializer.Serialize(EmpList);
+
+var StudentJson = JsonSerializer.Serialize(studentList);
+
+//File.WriteAllText(@"D:\Employee.json", EmpList.ToString());
+//File.WriteAllText(@"D:\Student.json", studentList.ToString());
+
+await using FileStream createStream = File.Create(@"D:\Employees.json");
+await JsonSerializer.SerializeAsync(createStream, EmpList);
+
+await using FileStream createStream1 = File.Create(@"D:\Students.json");
+await JsonSerializer.SerializeAsync(createStream1, studentList);
---
+++ ElasticSearchConnectivity/ClnsertElastic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticSearchConnectivity
+{
+    public class ClnsertElastic
+    {
+        //public ElasticClient OpenDBConn(string defaultindex)
+        //{
+        //    string IP = System.Configuration.ConfigurationManager.AppSettings.Get("uri");
+        //    var node = new Uri(IP);
+        //    var settings = new ConnectionSettings(node);
+        //    // I like exceptions
+        //    settings.ThrowExceptions(alwaysThrow: true);
+        //    // Good for DEBUG
+        //    settings.PrettyJson();
+        //    var client = new ElasticClient(settings);
+        //    //DB or Table Selection
+        //    settings.DefaultIndex(defaultindex);
+        //    return client;
+        //}
+    }
+}
