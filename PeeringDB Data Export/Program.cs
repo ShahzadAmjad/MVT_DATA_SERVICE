0a470794084080066ff63561cad6eab589b2d3ef
Shehzad Amjad <74287706+ShahzadAmjad@users.noreply.github.com>
2022-06-20 16:41:49 +0500

    in progress2

--- PeeringDB Data Export/Program.cs
+++ PeeringDB Data Export/Program.cs
@@ -18,9 +18,9 @@ foreach (var collectionName in collectionList)
     
     if (collectionName == "pdb_datacenters")
     {
-        Console.WriteLine("Getting all data for: "+collectionName);
-        List<Cpdb_tranformation> pdbData_List = new List<Cpdb_tranformation>();
-        var docList = mongodb.GetCollection( collectionName);
+        Console.WriteLine("Getting all data for: " + collectionName);
+        //List<Cpdb_tranformation> pdbData_List = new List<Cpdb_tranformation>();
+        //pdbData_List = mongodb.GetCollection( collectionName);
         //foreach (var doc in pdbData_List)
         //{
         //    //TO DO
@@ -29,62 +29,62 @@ foreach (var collectionName in collectionList)
     else if (collectionName == "pdb_internet_exchanges")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_InternetExchange> pdbData_List = new List<pdb_InternetExchange>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_InternetExchange> pdbData_List = new List<pdb_InternetExchange>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_internet_exchange_facilities")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_InternetExchangeFacility> pdbData_List = new List<pdb_InternetExchangeFacility>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_InternetExchangeFacility> pdbData_List = new List<pdb_InternetExchangeFacility>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_internet_exchange_networks")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_internet_exchange_networks> pdbData_List = new List<pdb_internet_exchange_networks>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_internet_exchange_networks> pdbData_List = new List<pdb_internet_exchange_networks>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_internet_exchange_prefixes")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_internet_exchange_prefixes> pdbData_List = new List<pdb_internet_exchange_prefixes>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_internet_exchange_prefixes> pdbData_List = new List<pdb_internet_exchange_prefixes>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_networks")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_Network> pdbData_List = new List<pdb_Network>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_Network> pdbData_List = new List<pdb_Network>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_network_pocs")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_NetworkPOC> pdbData_List = new List<pdb_NetworkPOC>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_NetworkPOC> pdbData_List = new List<pdb_NetworkPOC>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_network_facilities")
     {
@@ -99,31 +99,31 @@ foreach (var collectionName in collectionList)
     else if (collectionName == "pdb_network_to_ix_connection")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_NetworkToIXConnection> pdbData_List = new List<pdb_NetworkToIXConnection>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_NetworkToIXConnection> pdbData_List = new List<pdb_NetworkToIXConnection>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_organizations")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_Organization> pdbData_List = new List<pdb_Organization>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_Organization> pdbData_List = new List<pdb_Organization>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
     else if (collectionName == "pdb_as_set")
     {
         Console.WriteLine("Getting all data for: " + collectionName);
-        List<pdb_AS_SET> pdbData_List = new List<pdb_AS_SET>();
-        pdbData_List = mongodb.GetCollection(collectionName);
-        foreach (var doc in pdbData_List)
-        {
-            //TO DO
-        }
+        //List<pdb_AS_SET> pdbData_List = new List<pdb_AS_SET>();
+        //pdbData_List = mongodb.GetCollection(collectionName);
+        //foreach (var doc in pdbData_List)
+        //{
+        //    //TO DO
+        //}
     }
 }
